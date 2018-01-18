87238c61a58690276d1bce918ea49d7bbc0997d9
Max Wolf <maxwolf@Maxs-MacBook-Pro.local>
2018-01-18 11:42:20 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace escaperoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
